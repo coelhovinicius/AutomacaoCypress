7ad9896509fb83ec8c289c0d64a56510fb5d444f
coelhovinicius <coelhovinicius@yahoo.com.br>
2024-10-15 16:46:00 -0300

    Testando Mochawesome

--- Cypress/runTests.bat
+++ Cypress/runTests.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Executando testes Cypress...
-node C:\Users\coelh\Pessoal\TI\CURSOS\AutomacaoDeTestes\AprendendoATestar\runTests.js
+node C:\Users\coelh\Pessoal\TI\CURSOS\AutomacaoDeTestes\Cypress\runTests.js
 
 echo Testes Cypress concluídos.
 pause
